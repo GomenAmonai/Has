f324062266c7838e4f8864b801f11ef1294d4785
Daniil <daniil.lednik000@mail.ru>
2025-04-28 16:09:11 +0300

    NP2 Хрипунов

---
+++ Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Client;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите почтовый индекс : ");
+        string postalCode = Console.ReadLine();
+        
+        TcpClient client = new TcpClient();
+        client.Connect("127.0.0.1", 8888);
+        NetworkStream stream = client.GetStream();
+        
+        byte[] data = Encoding.UTF8.GetBytes(postalCode);
+        stream.Write(data, 0, data.Length);
+        
+        byte[] buffer = new byte[1024];
+        int byteCount = stream.Read(buffer, 0, buffer.Length);
+        string response = Encoding.UTF8.GetString(buffer, 0, byteCount);
+
+        Console.WriteLine("Названия улиц: ");
+        Console.WriteLine(response);
+        
+        client.Close();
+    }
+}
---
+++ NP2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NP2;
+
+class Program
+{
+    const int Port = 8888;
+    static void Main(string[] args)
+    {
+        TcpListener server = new TcpListener(IPAddress.Loopback, Port);
+        server.Start();
+        Console.WriteLine($"Listening on port {Port}");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Client connected");
+            
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+            int byteCount = stream.Read(buffer, 0, buffer.Length);
+            string requestedCode = Encoding.UTF8.GetString(buffer, 0, byteCount);
+            Console.WriteLine($"Received request for {requestedCode}");
+            
+            List<string> streets = FindStreetsForCode(requestedCode);
+            
+            string response = streets.Count > 0 ? string.Join("\n", streets) : "No streets found"; 
+            byte[] responseData = Encoding.UTF8.GetBytes(response);
+            stream.Write(responseData, 0, responseData.Length);
+            
+            client.Close();
+            Console.WriteLine("Closing connection");
+        }
+    }
+
+    static List<string> FindStreetsForCode(string code)
+    {
+        var lines = File.ReadAllLines("streets.txt");
+        List<string> streets = new List<string>();
+        bool isCorrectSection = false;
+
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line)) continue;
+
+            if (line.Trim() == code)
+            {
+                isCorrectSection = true;
+                continue;
+            }
+            else if (line.Trim().All(char.IsDigit))
+            {
+                isCorrectSection = true;
+            }
+
+            if (isCorrectSection)
+            {
+                streets.Add(line.Trim());
+            }
+        }
+        return streets;
+    }
+}
